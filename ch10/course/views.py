c5516802158448ac6f50dc4cb1f66d353a1f79a7
BiragAcharya <Biragacharya2062@gmail.com>
2025-01-28 13:47:06 +0545

    Practice

---
+++ ch10/course/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def learn_django(req):
+    # return render(req, template_name, context=dic_name, 
+    #               context_type=MIME_TYPE, status=None, using=None)
+
+    return render(req, 'course/django.html')
+
+
+
+def learn_fastapi(req):
+    return render(req, 'course/fastapi.html')
