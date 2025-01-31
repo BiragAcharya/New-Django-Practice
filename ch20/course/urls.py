dde3412ef50c8ac3a0bb263bf69a9665ffb1db0a
BiragAcharya <Biragacharya2062@gmail.com>
2025-01-31 21:21:29 +0545

    Practice

---
+++ ch20/course/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from course.views import learn_django, learn_python
+
+
+urlpatterns = [
+    path('dj/', learn_django, name='django'),
+    path('py/', learn_python, name='python'),
+]
