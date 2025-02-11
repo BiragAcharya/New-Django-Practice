92e5b3eb9e5420abad0f9ee3d6de17cd37eddb0b
BiragAcharya <Biragacharya2062@gmail.com>
2025-02-11 23:29:25 +0545

    Practice

--- ch37djangoqr/ch37djangoqr/urls.py
+++ ch37djangoqr/ch37djangoqr/urls.py
@@ -22,3 +22,7 @@ urlpatterns = [
     path('core/', include('core.urls')),
     path('scanner/', include('scanner.urls')),
 ]
+
+#Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
